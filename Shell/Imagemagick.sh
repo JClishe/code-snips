372b3e96a5761efe26be2fbd718bc372ad9c9490
Jason Clishe <clishej@gmail.com>
2022-07-22 10:16:19 -0400

    Initial commit

---
+++ Shell/Imagemagick.sh
@@ -0,0 +1,6 @@
+convert testing.png -border 1x1 -bordercolor black result.png
+
+convert testing.png testing.jpg # not positive this is the correct syntax
+
+convert testing.png -resize 1920 newfile.png 
+
