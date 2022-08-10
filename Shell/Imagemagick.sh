7a7ee4ef81b30e2c2cac8f54811a7a0e03d8738b
Jason Clishe <clishej@gmail.com>
2022-08-10 15:12:14 -0400

    Updated imagemagick content

--- Shell/Imagemagick.sh
+++ Shell/Imagemagick.sh
@@ -4,3 +4,14 @@ convert testing.png testing.jpg # not positive this is the correct syntax
 
 convert testing.png -resize 1920 newfile.png 
 
+#convert all files in a directory
+for f in *.BMP; do convert "$f" "$f.jpg"; done
+
+#same conversion as above, in a script
+#!/bin/zsh
+cd /path/to/some/working/dir/
+for f in *.jpg
+do
+    /opt/homebrew/bin/convert "$f" "$f.heic"
+done
+
