302d6ced043a5a7eabfd8f8eb92ad1b113c4dac3
Jason Clishe <clishej@gmail.com>
2023-04-11 16:39:17 -0400

    Added some Python modules for future reference

---
+++ Python/Modules/top_20_ohio_cities.py
@@ -0,0 +1,5 @@
+ohio_cities = {'Columbus': 898553, 'Cleveland': 381009, 'Cincinnati': 303940, 'Toledo': 273683, 
+               'Akron': 197597, 'Dayton': 140939, 'Parma': 78965, 'Canton': 70132, 'Youngstown': 64980,
+               'Lorain': 63871, 'Hamilton': 62823, 'Springfield': 58932, 'Kettering': 55445, 
+               'Elyria': 53965, 'Lakewood': 50551, 'Middletown': 48252, 'Mansfield': 46433,
+               'Euclid': 47233, 'Newark': 47077, 'Mentor': 46552}
