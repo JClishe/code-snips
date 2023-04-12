712b0f0a9d9e10cf8384f52e067a8fd439d1a752
Jason Clishe <clishej@gmail.com>
2023-04-12 19:32:29 -0400

    Added 100 Days of Code, days 8 and 8

--- Python/100 Days of Code/day 06 hangman.py
+++ Python/100 Days of Code/day 06 hangman.py
@@ -1,58 +1,7 @@
 import random
-
-word_list = ["ardvark", "baboon", "camel"]
-
-stages = ['''
-  +---+
-  |   |
-  O   |
- /|\  |
- / \  |
-      |
-=========''', '''  
-+---+
-  |   |
-  O   |
- /|\  |
- /    |
-      |
-=========''', '''  
-  +---+
-  |   |
-  O   |
- /|\  |
-      |
-      |
-=========''', '''
-  +---+
-  |   |
-  O   |
- /|   |
-      |
-      |
-=========''', '''
-  +---+
-  |   |
-  O   |
-  |   |
-      |
-      |
-=========''', '''
-  +---+
-  |   |
-  O   |
-      |
-      |
-      |
-=========''', '''
-  +---+
-  |   |
-      |
-      |
-      |
-      |
-=========''']
-
+from day_06_hangman_word_list import word_list
+from day_06_hangman_art import stages
+ 
 #pick a random work from the word list
 chosen_word = random.choice(word_list)
 word_length = len(chosen_word)
@@ -65,22 +14,29 @@ for letter in range(word_length):
 
 end_of_game = False
 lives = 6
+guessed_letters = []
 
 while not end_of_game:
     #ask the user to guess a letter
     guess = input("Guess a letter: ").lower()
-
+    
+    if guess in guessed_letters:
+        print("You have already chosen that letter, please guess again")
+        guessed_letters.append(guess)
+    else:
     #replace the underscores in the display list with the correctly guessed letter at the appropriate position
-    for position in range(word_length):
-        letter = chosen_word[position]
-        if letter == guess:
-            display[position] = letter
+        for position in range(word_length):
+            letter = chosen_word[position]
+            if letter == guess:
+                display[position] = letter
+        #deduct a life if the user chooses a letter that's not in the chosen word
+        if guess not in chosen_word:
+            lives -= 1
+            print(stages[lives])
+            print("The letter you guessed is not in the chosen word, you lose a life. Please guess again")
+        guessed_letters.append(guess)
 
-    if guess not in chosen_word:
-        lives -= 1
-        print(stages[lives])
-
-    print(display)
+    print(f' '.join(display))
 
     if "_" not in display:
         end_of_game = True
---
+++ Python/100 Days of Code/day 08 functions with input.py
@@ -0,0 +1,6 @@
+def greet():
+    print(f"Hello")
+    print(f"How are you today?")
+    print(f"What would you like for dinner?")
+
+greet()
